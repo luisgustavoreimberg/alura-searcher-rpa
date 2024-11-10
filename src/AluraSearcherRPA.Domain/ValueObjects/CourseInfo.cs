1b3243ec1a5a156b504e8fad2b75b81d220e18ef
luisgustavoreimberg <luisgustavoreimberg@gmail.com>
2024-11-10 01:54:49 -0300

    Definitions of main project Entitties and ValueObjects

---
+++ src/AluraSearcherRPA.Domain/ValueObjects/CourseInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraSearcherRPA.Domain.ValueObjects
+{
+    public class CourseInfo
+    {
+        public required string Title { get; set; }
+        public string? Instructor { get; set; }
+        public string? Duration { get; set; }
+        public string? Description { get; set; }
+
+        public CourseInfo() { }
+        public CourseInfo(string title, string? instructor, string? duration, string? description)
+        {
+            Title = title;
+            Instructor = instructor;
+            Duration = duration;
+            Description = description;
+        }
+    }
+}
