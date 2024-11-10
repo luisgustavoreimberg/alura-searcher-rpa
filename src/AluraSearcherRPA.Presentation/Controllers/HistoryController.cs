2791f5384ff97e31ff841d35cb4b6ed2d3aade25
luisgustavoreimberg <luisgustavoreimberg@gmail.com>
2024-11-10 01:55:39 -0300

    Controllers definitions

---
+++ src/AluraSearcherRPA.Presentation/Controllers/HistoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AluraSearcherRPA.Presentation.Controllers
+{
+    [ApiController]
+    public class HistoryController : BaseController
+    {
+        [HttpGet]
+        public IActionResult GetAllHistory()
+        {
+            return Ok();
+        }
+
+        [HttpGet("by-id/{historyId}")]
+        public IActionResult GetHistoryById([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("by-value/{searchedValue}")]
+        public IActionResult GetHistoryBySearchValue([FromRoute] string searchedValue)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/AluraSearcherRPA.Presentation/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AluraSearcherRPA.Presentation.Controllers
+{
+    [ApiController]
+    public class SearchController : BaseController
+    {
+        [HttpGet("{valueToSearch}")]
+        public IActionResult ExecuteSearch([FromRoute] string valueToSearch)
+        {
+            if (string.IsNullOrWhiteSpace(valueToSearch))
+                return BadRequest();
+            else
+                return Ok();
+        }
+    }
+}
