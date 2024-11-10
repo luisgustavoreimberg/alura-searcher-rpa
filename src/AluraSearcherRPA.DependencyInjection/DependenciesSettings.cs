36a4342598db66636e226463734dd8413c7b4fe8
luisgustavoreimberg <luisgustavoreimberg@gmail.com>
2024-11-10 01:56:27 -0300

    Initial dependecies settings setup

---
+++ src/AluraSearcherRPA.DependencyInjection/DependenciesSettings.cs
@@ -0,0 +1,18 @@
+﻿using AluraSearcherRPA.Application.Interfaces;
+using AluraSearcherRPA.Application.Services;
+using AluraSearcherRPA.RPA.Interfaces;
+using AluraSearcherRPA.RPA.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AluraSearcherRPA.DependencyInjection
+{
+    internal static class DependenciesSettings
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IHistoryService, HistoryService>();
+            services.AddScoped<ISearchService, SearchService>();
+            services.AddScoped<IAutomationService, AluraAutomationService>();
+        }
+    }
+}
